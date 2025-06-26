0ddb2b672c3fb290fe22f7c79c7e5b10a3a2274c
Dima4250 <dmitriy.vorobiev2016@yandex.ru>
2025-06-26 17:56:53 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Базовый интерфейс прототипа
+public interface IPrototype
+{
+    IPrototype Clone();
+}
+
+// Конкретный класс для клонирования
+public class ConcretePrototype : IPrototype
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public IPrototype Clone()
+    {
+        // Поверхностное копирование
+        return (IPrototype)this.MemberwiseClone();
+    }
+}
+
+// Использование
+class Program
+{
+    static void Main()
+    {
+        var original = new ConcretePrototype { Id = 1, Name = "Оригинал" }; // экземпляр класса
+        var clone = (ConcretePrototype)original.Clone();
+
+        clone.Name = "Клон";
+        Console.WriteLine($"Оригинал: {original.Name}"); // Original
+        Console.WriteLine($"Клон: {clone.Name}");       // Clone
+    }
+}
